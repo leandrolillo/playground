543cea21c71d8723b853438194275ea0aef60d59
Leandro Lillo <llillo@medallia.com>
2026-02-03 17:59:05 -0300

    Adding json parser tests

--- test/src/playground/playgroundTests.cpp
+++ test/src/playground/playgroundTests.cpp
@@ -1,8 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include "chrono"
 #include "thread"
 
+#include "mathMatchers.h"
 #include "PlaygroundMock.h"
 #include"JsonParser.h"
 
@@ -130,7 +132,54 @@ TEST_CASE("Parsers") {
   }
 
   SECTION("JsonParser") {
+    using namespace Catch::Matchers;
+
     FileParser fileParser("resources/jsonTestFile.json");
     JsonParser parser(fileParser);
+
+    String token;
+    parser.readStartObject();
+    while ((token = parser.readToken()) != END_OBJECT && token != FileParser::eof) {
+      parser.readValueSeparator();
+
+      if(token == "stringProperty") {
+        CHECK("value" == parser.readString());
+      } else if(token == "integerProperty") {
+        CHECK(-10 == parser.readInteger());
+      } else if(token == "unsignedIntegerProperty") {
+        CHECK(10 == parser.readUnsignedInteger());
+      } else if(token == "realProperty") {
+        CHECK_THAT(parser.readReal(), Catch::Matchers::WithinRel(1e-12, 0.001));
+      } else if(token == "booleanProperty") {
+        CHECK(true == parser.readBoolean());
+      } else if(token == "vector2Property") {
+        CHECK_THAT(parser.readVector2(), EqualsVector(vector2(1, 2)));
+      } else if(token == "vector3Property") {
+        CHECK_THAT(parser.readVector3(), EqualsVector(vector3(3, 4, 5)));
+      } else if(token == "stringList") {
+        CHECK_THAT(parser.readStringArray(), Equals(std::vector<String>{"one", "two", "three"}));
+      } else if(token == "integerList") {
+        CHECK_THAT(parser.readIntegerArray(), Equals(std::vector<int> {-3, -2, -1, 0, 1, 2, 3}));
+      } else if(token == "unsignedIntegerList") {
+        CHECK_THAT(parser.readUnsignedIntegerArray(), Equals(std::vector<unsigned int> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+      } else if(token == "realList") {
+        CHECK_THAT(parser.readRealArray(), Equals(std::vector<real> {1.1, 1.2, 1.3, 1.4, 1.5, 1.0E12, 1.0e-12, 1.6, 1.7, 1. }));
+      } else if(token == "vector2List") {
+        parser.readVector2Array();
+        //TODO: Check array of vector2
+        //CHECK_THAT(parser.readVector2Array(), Equals(std::vector<vector2> {vector2(1.0, 2.0), vector2(3.0, 4.0)}));
+      } else if(token == "vector3List") {
+        parser.readVector3Array();
+        //TODO: Check array of vector3
+        //CHECK_THAT(parser.readVector3Array(), Equals(std::vector<vector3> {vector3(1.0, 2.0, 3.0), vector3(4.0, 5.0, 6.0)}));
+      } else {
+        //TODO: We should support a way of reading and discarding unused properties
+      }
+
+
+      if (parser.peekToken() == ",") {
+        parser.readToken();
+      }
+    }
   }
 }
