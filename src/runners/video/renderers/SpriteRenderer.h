9a8adee0862de98751561c91f71ba225ccdf9e96
Leandro Lillo <llillo@medallia.com>
2026-03-09 22:52:54 -0300

    Troubleshooting text renderer performance

--- src/runners/video/renderers/SpriteRenderer.h
+++ src/runners/video/renderers/SpriteRenderer.h
@@ -103,7 +103,7 @@ public:
 protected:
   void doRender(const Camera &camera) override {
     video.enable(VideoAttribute::BLEND, VideoAttribute::SRC_ALPHA, VideoAttribute::ONE_MINUS_SRC_ALPHA);
-    video.disable(VideoAttribute::DEPTH_TEST);
+    video.enable(VideoAttribute::DEPTH_TEST); //Defered rendering by texture eliminates ordering thus background is rendered before foreground without depth_test
     video.enable(VideoAttribute::CULL_FACE, VideoAttribute::BACK);
 
     video.sendMatrix("matrices.projectionView", camera.getProjectionMatrix() * matriz_4x4::traslacion(camera.getPosition()));
